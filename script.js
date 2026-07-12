bf11132e0bd42041882357ec1307ae7ec3db7eff
igornogueiraapp-del <igornogueiraapp@gmail.com>
2026-07-12 02:32:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-// RideTune JavaScript
+const languages = document.querySelectorAll(".language");
+const musicScreen = document.getElementById("musicScreen");
+
+languages.forEach(language => {
+  language.addEventListener("click", () => {
+    musicScreen.classList.add("active");
+  });
+});
