652f73de68c55604117589d6571f712174a0947f
igornogueiraapp-del <igornogueiraapp@gmail.com>
2026-07-12 16:37:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// Abre a tela de músicas
 function openMusic() {
-  document.getElementById("musicScreen").classList.add("active");
+    document.getElementById("languageScreen").style.display = "none";
+    document.getElementById("musicScreen").style.display = "block";
+}
+
+// Seleciona uma música
+function selectMusic(card) {
+
+    // Remove a seleção de todos os cards
+    const cards = document.querySelectorAll(".music-card");
+
+    cards.forEach(function(item) {
+        item.classList.remove("selected");
+    });
+
+    // Marca apenas o card clicado
+    card.classList.add("selected");
 }
