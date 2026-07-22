2e66c980637fe3c4973560de12ec90cdd2d9ed46
igornogueiraapp-del <igornogueiraapp@gmail.com>
2026-07-22 15:16:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,9 @@
 // ==========================
-// FIREBASE CONFIGURAÇÃO
+// FIREBASE
 // ==========================
 
-// Cole aqui os dados do seu Firebase
-const firebaseConfig = {
-    apiKey: "SUA_API_KEY",
-    authDomain: "SEU_PROJETO.firebaseapp.com",
-    projectId: "SEU_PROJECT_ID",
-    storageBucket: "SEU_PROJETO.appspot.com",
-    messagingSenderId: "SEU_SENDER_ID",
-    appId: "SEU_APP_ID"
-};
-
-
-// Inicializar Firebase
 firebase.initializeApp(firebaseConfig);
 
-
-// Banco Firestore
 const db = firebase.firestore();
 
 
