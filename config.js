76dc9b47430d31da7728fdeb54230ad024661df0
igornogueiraapp-del <igornogueiraapp@gmail.com>
2026-07-22 14:51:09 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,11 @@
-const YOUTUBE_API_KEY = "AIzaSyBZsl1VLGmfFJ3J1MkS2F9FnsQCWQAFdJw";
+const YOUTUBE_API_KEY = "SUA_CHAVE_DO_YOUTUBE";
+
+
+const firebaseConfig = {
+    apiKey: "SUA_API_KEY_DO_FIREBASE",
+    authDomain: "SEU_PROJETO.firebaseapp.com",
+    projectId: "SEU_PROJECT_ID",
+    storageBucket: "SEU_PROJETO.appspot.com",
+    messagingSenderId: "SEU_MESSAGING_SENDER_ID",
+    appId: "SEU_APP_ID"
+};
